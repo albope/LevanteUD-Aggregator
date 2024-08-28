947e38035e5d5d607b7f56259ebb8a370d78d9cf
albope <albertobort@gmail.com>
2024-08-28 09:30:16 +0200

    Se integra resultados y clasificacion

--- src/components/ResultadoPartido.js
+++ src/components/ResultadoPartido.js
@@ -3,31 +3,15 @@ import './ResultadoPartido.css';
 
 function ResultadoPartido() {
   return (
-    <div className="partidos-clasificacion">
-      <div className="resultado-partido widget-container">
-        <h2>Resultados en Vivo</h2>
-        <iframe
-          id="iframe_220_1724760382"
-          name="iframe_220_1724760382"
-          src="https://widgets.elpais.com/liga/2023_2024/widget_liga.html?c=azul&w=220&h=555&d=primera"
-          width="220"
-          height="555"
-          frameBorder="0"
-          scrolling="auto"
-          style={{ marginBottom: '10px' }}
-          title="Resultados en Vivo"
-        ></iframe>
-      </div>
-      <div className="resultado-partido clasificacion">
-        <h2>Clasificación</h2>
-        <ul className="liga-table">
-          <li>1. Equipo A</li>
-          <li>2. Equipo B</li>
-          <li>3. Equipo C</li>
-          <li>4. Levante UD</li>
-          <li>5. Equipo D</li>
-        </ul>
-      </div>
+    <div className="resultado-partido">
+      <h2>Resultados en Vivo</h2>
+      <iframe 
+        src="https://www.lavanguardia.com/deportes/resultados/laliga-segunda-division" 
+        width="100%" 
+        height="600px" 
+        style={{ border: 'none' }}
+        title="Resultados La Liga Segunda División"
+      ></iframe>
     </div>
   );
 }
